ea5ac98f299bcb5780c46726938e6913365874ee
Salimeh Birang <salimeh.s@gmail.com>
2020-02-11 13:07:32 -0600

    completedPS4

---
+++ Problemsets/PS4_Birang/PS4b_Birang.R
@@ -0,0 +1,11 @@
+df <- createDataFrame(iris)
+df1 <- iris
+
+class(df)
+class(df1)
+
+head(select(df, df$Sepal_Length, df$Species)) 
+head(filter(df, df$Sepal_Length>5.5))
+head(select(df, df$Sepal_Length, df$Species), filter(df, df$Sepal_Length>5.5)))
+head(summarize(groupBy(df, df$Species),mean=mean(df$Sepal_Length), count=n(df$Sepal_Length)))
+
